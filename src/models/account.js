cb4c8c7f942f0ea60fb5101ee714d47fe201fc9d
MacBook <macbook@MacBooks-MacBook-Pro-7.local>
2020-02-09 20:43:05 +0700

    fix email

---
+++ src/models/account.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+const accountSchema = new mongoose.Schema(
+    {
+        startup_name:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        startup_category:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        full_name:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        job_position:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        email:{
+            type:String,
+            required:true,
+            lowercase:true
+        },
+        password:{
+            type:String,
+            required:true,
+            minlength:6,
+            trim:true
+        },
+        token:{
+            type:String,
+            required:true
+        },
+        tokens:[
+            {
+                token:{
+                    type:String,
+                    required:true
+                }
+            }
+        ],
+        confirmed:{
+            type:Boolean,
+            default:false
+        }
+    }
+)
+
+
+accountSchema.methods.generateAuthToken= async function(){
+    const account = this
+    // console.log(account,'halo')
+    const token = jwt.sign({_id:account._id.toString()},'my_secret')
+
+    account.tokens = account.tokens.concat({token})
+    account.token = token
+    await account.save()
+
+    return token
+}
+
+
+
+accountSchema.methods.updateAuthToken = async function(id){
+    console.log(id)
+}
+
+const Account = mongoose.model('Account',accountSchema)
+module.exports = Account
---
+++ src/routers/account.js
@@ -0,0 +1,186 @@
+const express = require('express')
+const Account = require('../models/account')
+const router = new express.Router()
+const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
+const ObjectId = require('mongodb').ObjectID
+const jwt = require('jsonwebtoken')
+
+
+router.post('/register',async(req,res)=>{
+    // console.log(req.body)
+    if(!req.body){
+        return res.status(400).send("Request Body is Empty")
+    }
+
+    //Check if user already exists
+    Account.findOne({
+        email:req.body.email
+    }).then(doc=>{
+        if(doc){
+            res.status(200).json({
+                token:"",
+                error:`The email address ${doc.email} is already in use`
+            })
+        }else{
+            try{
+                // console.log(req.body)
+                const account = new Account({
+                    ...req.body
+                })
+
+
+                // console.log(account)
+                bcrypt.genSalt(10,(err,salt)=>{
+                    bcrypt.hash(account.password,salt,async (err,hash)=>{
+                        if(err) throw err;
+                        account.password = hash;
+                       const token =  await account.generateAuthToken()
+
+                        account.save().then(account=>{
+                            console.log(account._id, "akun")
+                            try{
+                                const transporter = nodemailer.createTransport({
+                                    service : "gmail",
+                                    // host: 'smtp.gmail.com',
+                                    // port: 465,
+                                    // secure: false,
+                                    auth:{
+                                        user:"donialuzzam@gmail.com",
+                                        pass:"050617Islam"
+                                    }
+                                })
+    
+                                const mailOptions = {
+                                    from:'donialuzzam@gmail.com',
+                                    to:account.email,
+                                    subject:"Verify Account",
+                                    text:"account",
+                                    // html:"<h1>hello</h1>"
+                                    html: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/verification\/' + account._id + '\/' + token + '.\n'
+                                }
+                                console.log(token, "token")
+                                transporter.sendMail(mailOptions,(error,info)=>{
+                                    console.log(mailOptions,'masuk')
+    
+                                    if(error){
+                                        console.log(error,'error')
+                                    }else{
+                                        console.log(info,'info')
+                                    }
+                                })
+    
+    
+                                res.status(201).send({account,token})
+                            }catch(e){
+                                console.log(e)
+                                res.status(400).send(e)
+
+                            }
+                        })
+                        
+                    })
+                })
+            }catch(e){
+                res.status(400).send(e)
+            }
+
+
+        }
+    })
+    
+
+    
+})
+
+router.get('/verification/:id/:token',async(req,res)=>{
+    const id = req.params.id
+    const token = req.params.token 
+
+    console.log(token,'token')
+    Account.findOne({
+        "_id":ObjectId(id),
+    }).then(doc=>{
+        jwt.verify(token,'my_secret',async function(err,token){
+            if(err){
+                res.status(400).send(err)
+            }else{
+                if(doc.confirmed){
+                    res.status(400).send({
+                        "success":false,
+                        "message":"Your Email Already verify, Please Login"
+                    })
+                }else{
+                    // const update = await Account.where({ "_id":ObjectId(id)}).update({ confirmed: true })
+                    const update = await Account.findOneAndUpdate({ "_id":ObjectId(id)},{confirmed:true})
+                    console.log(update)
+                    const newAccount = await Account.findOne({"_id":ObjectId(id)})
+                    res.status(200).send({
+                        success:true,
+                        message:"Please Login",
+                        data:newAccount
+                    })
+                }
+            }
+        })
+    })
+})
+
+router.get('/resend/:id',async(req,res)=>{
+    const id = req.params.id
+    Account.findOne({
+        "_id":ObjectId(id)
+    }).then(async doc=>{
+        if(doc.confirmed){
+            res.status(200).json({
+                error:`The email address ${doc.email} is already in use`
+            })
+        }else{
+            try{
+                
+                const token = doc.generateAuthToken();
+                const account = await Account.findOne({"_id":ObjectId(id)})
+                console.log(account,'account')                
+                const url = `http://localhost:4000/verification/${id}/${token}`
+
+
+
+                const transporter = nodemailer.createTransport({
+                    service:"gmail",
+                    auth:{
+                        user:"rizal.prasetya11@gmail.com",
+                        pass:"rizal123"
+                    }
+                })
+
+
+                const mailOptions={
+                    from:"rizal.prasetya11@gmail.com",
+                    to:"rizalprasetya22@gmail.com",
+                    subject:"Resend Verification",
+                    text:"account",
+                    html:"Please verification your account <a href="+url+">link</a>"
+                }
+
+                transporter.sendMail(mailOptions,(error,info)=>{
+                    if(error){
+                        console.log(error,'error')
+                    }else{
+                        console.log(info,'info')
+                    }
+                    
+                })
+
+                
+
+                console.log(account,'account')
+                res.status(201).send({account,token})
+            }catch(e){
+                console.log(e)
+            }
+
+        }
+    })
+})
+
+module.exports = router
